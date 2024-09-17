7f4e3935d393e3cdf2d82e5c786311db6384fdeb
InvestUz <invest.abdusattorov@gmail.com>
2024-09-17 10:07:30 +0500

    vue js with cdn has been connected

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, jsonify
+from flask_caching import Cache
 
 app = Flask(__name__)
 
-# Initial state
-user_earnings = 0
+# Setup cache config (to optimize performance)
+app.config['CACHE_TYPE'] = 'SimpleCache'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 300
+cache = Cache(app)
+
+# Initial state stored in cache
+cache.set('user_earnings', 0)
 
 @app.route('/')
 def index():
@@ -11,8 +17,9 @@ def index():
 
 @app.route('/click', methods=['POST'])
 def click():
-    global user_earnings
+    user_earnings = cache.get('user_earnings') or 0
     user_earnings += 1  # Increment earnings for each click
+    cache.set('user_earnings', user_earnings)  # Update the cache
     return jsonify({'earnings': user_earnings})
 
 if __name__ == '__main__':
